bd33221f44f0bd271ec11db5566bcd1c5a2c2c57
Rian Priansyah <rian.priansyah04@gmail.com>
2023-07-23 20:35:36 +0700

    menampilkan seluruh data collection firestore dalam vue

--- src/composable/getPosts.js
+++ src/composable/getPosts.js
@@ -1,4 +1,5 @@
 import { ref } from "vue";
+import { projectFirestore } from "@/firebase/config";
 
 const getPosts = () => {
   const posts = ref([]);
@@ -6,15 +7,13 @@ const getPosts = () => {
 
   const load = async () => {
     try {
-      await new Promise((resolve) => {
-        setTimeout(resolve, 2000);
+      const res = await projectFirestore.collection("posts").get();
+      posts.value = res.docs.map((doc) => {
+        return {
+          ...doc.data(),
+          id: doc.id,
+        };
       });
-
-      let data = await fetch("http://localhost:3000/posts");
-      if (!data.ok) {
-        throw Error("Tidak ada data");
-      }
-      posts.value = await data.json();
     } catch (err) {
       error.value = err.message;
     }
