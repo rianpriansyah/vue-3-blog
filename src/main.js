87dbfcb98971e343db17dab7344849f1b5b5df6a
Rian Priansyah <rian.priansyah04@gmail.com>
2023-07-23 09:53:06 +0700

    menginstal library pihak ketiga untuk template blog

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-createApp(App).use(router).mount('#app')
+/* Bootstrap */
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+
+/* jquery */
+import jQuery from "jquery";
+window.$ = jQuery;
+
+/* fontawesome */
+import "@fortawesome/fontawesome-free/css/all.css";
+import "@fortawesome/fontawesome-free/js/all.js";
+
+createApp(App).use(router).mount("#app");
