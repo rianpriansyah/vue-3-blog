0ab87865939e7965b770a07366aa131289750ca1
Rian Priansyah <rian.priansyah04@gmail.com>
2023-07-24 10:35:49 +0700

    menambah data collection dari vue

--- src/views/posts/Create.vue
+++ src/views/posts/Create.vue
@@ -44,6 +44,7 @@
 <script>
 import { ref } from "@vue/reactivity";
 import { useRouter } from "vue-router";
+import { projectFirestore } from "@/firebase/config";
 
 export default {
   setup() {
@@ -69,11 +70,7 @@ export default {
         tags: tags.value,
       };
 
-      await fetch("http://localhost:3000/posts/", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(post),
-      });
+      const res = await projectFirestore.collection("posts").add(post);
 
       router.push({
         name: "Home",
